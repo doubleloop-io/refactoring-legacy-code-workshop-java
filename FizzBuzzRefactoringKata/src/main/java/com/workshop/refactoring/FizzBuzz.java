17e1988912dd05dbc3e6b74d2b47422afb57dad8
Matteo Baglini <matteo.baglini@gmail.com>
2017-05-08 17:21:14 +0200

    Move local variable outer scope

--- FizzBuzzRefactoringKata/src/main/java/com/workshop/refactoring/FizzBuzz.java
+++ FizzBuzzRefactoringKata/src/main/java/com/workshop/refactoring/FizzBuzz.java
@@ -4,18 +4,16 @@ import java.util.Objects;
 
 public class FizzBuzz {
     public static String translate(Integer number) {
+        String result;
         if (number % 15 == 0) {
-            String result;
             result = "FizzBuzz";
             return  result;
         }
         if (number % 3 == 0) {
-            String result;
             result = "Fizz";
             return  result;
         }
         if (number % 5 == 0) {
-            String result;
             result = "Buzz";
             return result;
         }
