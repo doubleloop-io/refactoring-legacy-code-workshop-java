97a00d56759b65da6b98aa8c7675b872777c0cdb
Matteo Baglini <matteo.baglini@gmail.com>
2017-05-08 20:36:40 +0200

    Extract path fields

--- TriviaRefactoringKata/src/test/java/com/workshop/refactoring/GoldenMasterTests.java
+++ TriviaRefactoringKata/src/test/java/com/workshop/refactoring/GoldenMasterTests.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.io.File;
 import java.io.PrintStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Random;
 
@@ -13,18 +14,20 @@ import static org.hamcrest.Matchers.is;
 
 public class GoldenMasterTests {
 
+    private final Path actualPath = Paths.get("output.log");
+    private final Path goldenMasterPath = Paths.get("golden-master.txt");
+
     @Test
     public void runGame() throws Exception {
-        File file = Paths.get("output.log").toFile();
-        System.setOut(new PrintStream(file));
+        System.setOut(new PrintStream(actualPath.toFile()));
         for (int i = 0; i < 1000; i++) {
             final int seed = 34728 + 17 * i;
             GameRunner.run(new Random(seed));
         }
 
-        final String actual = String.valueOf(Files.readAllLines(Paths.get("output.log")));
-        final String expected = String.valueOf(Files.readAllLines(Paths.get("golden-master.txt")));
-        
+        final String actual = String.valueOf(Files.readAllLines(actualPath));
+        final String expected = String.valueOf(Files.readAllLines(goldenMasterPath));
+
         assertThat(actual, is(expected));
     }
 }
