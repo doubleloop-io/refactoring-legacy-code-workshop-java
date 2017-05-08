94c742468de212d70a8500d1ccd6e7b487298fd4
Matteo Baglini <matteo.baglini@gmail.com>
2017-05-08 17:20:10 +0200

    Split local variables declarations from initializations

--- FizzBuzzRefactoringKata/src/main/java/com/workshop/refactoring/FizzBuzz.java
+++ FizzBuzzRefactoringKata/src/main/java/com/workshop/refactoring/FizzBuzz.java
@@ -5,15 +5,18 @@ import java.util.Objects;
 public class FizzBuzz {
     public static String translate(Integer number) {
         if (number % 15 == 0) {
-            String result = "FizzBuzz";
+            String result;
+            result = "FizzBuzz";
             return  result;
         }
         if (number % 3 == 0) {
-            String result = "Fizz";
+            String result;
+            result = "Fizz";
             return  result;
         }
         if (number % 5 == 0) {
-            String result = "Buzz";
+            String result;
+            result = "Buzz";
             return result;
         }
         return Objects.toString(number);
