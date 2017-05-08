a1b7741e94e18ec0f75d119e083e06ffc751c999
Matteo Baglini <matteo.baglini@gmail.com>
2017-05-08 17:26:10 +0200

    Leave only one return

--- FizzBuzzRefactoringKata/src/main/java/com/workshop/refactoring/FizzBuzz.java
+++ FizzBuzzRefactoringKata/src/main/java/com/workshop/refactoring/FizzBuzz.java
@@ -5,18 +5,11 @@ import java.util.Objects;
 public class FizzBuzz {
     public static String translate(Integer number) {
         String result = "";
-        if (number % 15 == 0) {
-            result = "FizzBuzz";
-            return result;
-        }
-        else if (number % 3 == 0) {
-            result = "Fizz";
-            return result;
-        }
-        else if (number % 5 == 0) {
-            result = "Buzz";
-            return result;
-        }
+
+        if (number % 15 == 0) result = "FizzBuzz";
+        else if (number % 3 == 0) result = "Fizz";
+        else if (number % 5 == 0) result = "Buzz";
+
         if (Objects.equals(result, ""))
             return Objects.toString(number);
         else
