f25d963ba53a354f8b346a24bf8f632913330609
Matteo Baglini <matteo.baglini@gmail.com>
2017-05-08 17:30:17 +0200

    Remove specific ifs and compose logic

--- FizzBuzzRefactoringKata/src/main/java/com/workshop/refactoring/FizzBuzz.java
+++ FizzBuzzRefactoringKata/src/main/java/com/workshop/refactoring/FizzBuzz.java
@@ -6,9 +6,8 @@ public class FizzBuzz {
     public static String translate(Integer number) {
         String result = "";
 
-        if (number % 15 == 0) result += "FizzBuzz";
-        else if (number % 3 == 0) result += "Fizz";
-        else if (number % 5 == 0) result += "Buzz";
+        if (number % 3 == 0) result += "Fizz";
+        if (number % 5 == 0) result += "Buzz";
 
         if (Objects.equals(result, ""))
             return Objects.toString(number);
