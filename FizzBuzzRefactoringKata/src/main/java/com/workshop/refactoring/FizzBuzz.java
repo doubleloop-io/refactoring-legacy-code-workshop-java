617243aa75caa958415fe3727fc4a32a08793136
Matteo Baglini <matteo.baglini@gmail.com>
2017-05-08 17:32:11 +0200

    Poperly support 7

--- FizzBuzzRefactoringKata/src/main/java/com/workshop/refactoring/FizzBuzz.java
+++ FizzBuzzRefactoringKata/src/main/java/com/workshop/refactoring/FizzBuzz.java
@@ -8,6 +8,7 @@ public class FizzBuzz {
 
         if (number % 3 == 0) result += "Fizz";
         if (number % 5 == 0) result += "Buzz";
+        if (number % 7 == 0) result += "Yo!";
 
         if (Objects.equals(result, ""))
             return Objects.toString(number);
