2e4c71434cc838f0213b8a4d94f351478adc0124
Matteo Baglini <matteo.baglini@gmail.com>
2017-05-08 20:14:38 +0200

    Use GameRunner::run in spike test with fixed seed

--- TriviaRefactoringKata/src/test/java/com/workshop/refactoring/SpikeTests.java
+++ TriviaRefactoringKata/src/test/java/com/workshop/refactoring/SpikeTests.java
@@ -5,12 +5,13 @@ import org.junit.Test;
 import java.io.File;
 import java.io.PrintStream;
 import java.nio.file.Paths;
+import java.util.Random;
 
 public class SpikeTests {
     @Test
     public void runGame() throws Exception {
         File file = Paths.get("output.log").toFile();
         System.setOut(new PrintStream(file));
-        GameRunner.main(null);
+        GameRunner.run(new Random(8));
     }
 }
