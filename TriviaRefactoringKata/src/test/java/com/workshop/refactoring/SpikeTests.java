21aefdda65e5ea9ca4b9f9398ec49d8aafabc8bf
Matteo Baglini <matteo.baglini@gmail.com>
2017-05-08 19:14:36 +0200

    Capture game output in memory

--- TriviaRefactoringKata/src/test/java/com/workshop/refactoring/SpikeTests.java
+++ TriviaRefactoringKata/src/test/java/com/workshop/refactoring/SpikeTests.java
@@ -2,9 +2,18 @@ package com.workshop.refactoring;
 
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class SpikeTests {
     @Test
     public void runGame() throws Exception {
-      GameRunner.main(null);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        GameRunner.main(null);
+        assertThat("", is(out.toString()));
     }
 }
