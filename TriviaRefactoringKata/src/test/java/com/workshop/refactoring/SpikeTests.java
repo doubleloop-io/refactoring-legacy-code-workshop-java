01a0a9bdf26571898bfd43b02bb9ec20adf54e56
Matteo Baglini <matteo.baglini@gmail.com>
2017-05-08 19:11:04 +0200

    Run game in test

---
+++ TriviaRefactoringKata/src/test/java/com/workshop/refactoring/SpikeTests.java
@@ -0,0 +1,10 @@
+package com.workshop.refactoring;
+
+import org.junit.Test;
+
+public class SpikeTests {
+    @Test
+    public void runGame() throws Exception {
+      GameRunner.main(null);
+    }
+}
