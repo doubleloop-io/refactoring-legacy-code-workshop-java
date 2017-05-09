6609f2f258d9771f983ee410e4c25bee41a9f8a6
Matteo Baglini <matteo.baglini@gmail.com>
2017-05-09 18:44:20 +0200

    Convert test to JUnitParams

--- FizzBuzzRefactoringKata/src/test/java/com/workshop/refactoring/FizzBuzzTests.java
+++ FizzBuzzRefactoringKata/src/test/java/com/workshop/refactoring/FizzBuzzTests.java
@@ -1,39 +1,25 @@
 package com.workshop.refactoring;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
-import java.util.Arrays;
-import java.util.Collection;
 
 import static com.workshop.refactoring.FizzBuzz.translate;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-@RunWith(Parameterized.class)
+@RunWith(JUnitParamsRunner.class)
 public class FizzBuzzTests {
 
-    private final Integer number;
-    private final String expected;
-
-    public FizzBuzzTests(Integer number, String expected) {
-        this.number = number;
-        this.expected = expected;
-    }
-
-    @Parameterized.Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                {3, "Fizz"}, {3 * 2, "Fizz"},
-                {5, "Buzz"}, {5 * 2, "Buzz"},
-                {15, "FizzBuzz"}, {15 * 2, "FizzBuzz"},
-                {1, "1"}, {2, "2"}, {4, "4"}, {8, "8"},
-        });
-    }
-
     @Test
-    public void test() throws Exception {
+    @Parameters({
+            "3, Fizz", "6, Fizz",
+            "5, Buzz", "10, Buzz",
+            "15, FizzBuzz", "30, FizzBuzz",
+            "1, 1", "2, 2", "4, 4", "8, 8",
+    })
+    public void convertNumber(Integer number, String expected) throws Exception {
         assertThat(translate(number), is(expected));
     }
 }
