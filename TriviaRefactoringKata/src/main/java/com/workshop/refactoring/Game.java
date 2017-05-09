aad82491754c43ca1f326efbeae113a6033075e2
Matteo Baglini <matteo.baglini@gmail.com>
2017-05-09 16:13:01 +0200

    Extract local currentPlayerPlace in currentCategory

--- TriviaRefactoringKata/src/main/java/com/workshop/refactoring/Game.java
+++ TriviaRefactoringKata/src/main/java/com/workshop/refactoring/Game.java
@@ -93,15 +93,16 @@ public class Game {
 
 
     private String currentCategory() {
-        if (places[currentPlayer] == 0) return "Pop";
-        if (places[currentPlayer] == 4) return "Pop";
-        if (places[currentPlayer] == 8) return "Pop";
-        if (places[currentPlayer] == 1) return "Science";
-        if (places[currentPlayer] == 5) return "Science";
-        if (places[currentPlayer] == 9) return "Science";
-        if (places[currentPlayer] == 2) return "Sports";
-        if (places[currentPlayer] == 6) return "Sports";
-        if (places[currentPlayer] == 10) return "Sports";
+        final int currentPlayerPlace = places[currentPlayer];
+        if (currentPlayerPlace == 0) return "Pop";
+        if (currentPlayerPlace == 4) return "Pop";
+        if (currentPlayerPlace == 8) return "Pop";
+        if (currentPlayerPlace == 1) return "Science";
+        if (currentPlayerPlace == 5) return "Science";
+        if (currentPlayerPlace == 9) return "Science";
+        if (currentPlayerPlace == 2) return "Sports";
+        if (currentPlayerPlace == 6) return "Sports";
+        if (currentPlayerPlace == 10) return "Sports";
         return "Rock";
     }
 
