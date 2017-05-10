6d8d7c3f0b29a7aeedc92e12d92400971dbe0e45
Matteo Baglini <matteo.baglini@gmail.com>
2017-05-10 07:21:47 +0200

    Add one test for Pop place

---
+++ TriviaRefactoringKata/src/test/java/com/workshop/refactoring/QuestionDeckTests.java
@@ -0,0 +1,15 @@
+package com.workshop.refactoring;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class QuestionDeckTests {
+    @Test
+    public void popPlace() throws Exception {
+        final QuestionDeck deck = new QuestionDeck();
+        final String category = deck.currentCategory(0);
+        assertThat(category, is("Pop"));
+    }
+}
