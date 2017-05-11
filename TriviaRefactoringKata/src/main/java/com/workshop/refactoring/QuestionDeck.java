67854b18a0c85452f512ea8f719101f4e3808aa6
Matteo Baglini <matteo.baglini@gmail.com>
2017-05-11 23:56:04 +0200

    Store categories into Map

--- TriviaRefactoringKata/src/main/java/com/workshop/refactoring/QuestionDeck.java
+++ TriviaRefactoringKata/src/main/java/com/workshop/refactoring/QuestionDeck.java
@@ -1,16 +1,13 @@
 package com.workshop.refactoring;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class QuestionDeck {
-    private final ArrayList<CategoryQuestions> categories;
+    private final Map<String, CategoryQuestions> categories;
 
     public QuestionDeck() {
-        categories = new ArrayList<>();
+        categories = new HashMap<>();
     }
 
     private String createQuestion(String category, int index) {
@@ -19,29 +16,29 @@ public class QuestionDeck {
 
     void fillQuestions() {
         CategoryQuestions pop = new CategoryQuestions("Pop");
-        categories.add(pop);
+        categories.put(pop.getName(), pop);
         pop.placeOn(Arrays.asList(0, 4, 8));
 
         CategoryQuestions science = new CategoryQuestions("Science");
-        categories.add(science);
+        categories.put(science.getName(), science);
         science.placeOn(Arrays.asList(1, 5, 9));
 
         CategoryQuestions sports = new CategoryQuestions("Sports");
-        categories.add(sports);
+        categories.put(sports.getName(), sports);
         sports.placeOn(Arrays.asList(2, 6, 10));
 
         CategoryQuestions rock = new CategoryQuestions("Rock");
         rock.placeOn(Arrays.asList(3, 7, 11));
-        categories.add(rock);
+        categories.put(rock.getName(), rock);
 
-        categories.stream()
+        categories.values() .stream()
                 .flatMap(c -> IntStream.range(0, 50)
                         .mapToObj(i -> new Pair<>(c, createQuestion(c.getName(), i))))
                 .forEach(p -> p.left.addQuestion(p.right));
     }
 
     String categoryFor(int place) {
-        return categories.stream()
+        return categories.values().stream()
                 .filter(c -> c.isPlacedOn(place))
                 .findFirst()
                 .orElseThrow(() -> new UndefinedCategoryException(place))
@@ -49,7 +46,7 @@ public class QuestionDeck {
     }
 
     Object nextQuestion(String category) {
-        return categories.stream()
+        return categories.values().stream()
                 .filter(c -> Objects.equals(category, c.getName()))
                 .findFirst()
                 .orElseThrow(() -> new UnknownCategoryException(category))
@@ -58,13 +55,13 @@ public class QuestionDeck {
 
     public void placeOn(String category, List<Integer> places) {
         final CategoryQuestions categoryQuestions = new CategoryQuestions(category);
-        categories.add(categoryQuestions);
+        categories.put(category, categoryQuestions);
         categoryQuestions.placeOn(places);
     }
 
     public void addQuestion(String category, String question) {
         final CategoryQuestions categoryQuestions = new CategoryQuestions(category);
-        categories.add(categoryQuestions);
+        categories.put(category, categoryQuestions);
         categoryQuestions.addQuestion(question);
     }
 
