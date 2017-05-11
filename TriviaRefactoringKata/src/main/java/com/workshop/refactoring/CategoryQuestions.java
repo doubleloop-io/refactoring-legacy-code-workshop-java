8590acdd01a7eb611bd1980d533099f65b3eb65a
Matteo Baglini <matteo.baglini@gmail.com>
2017-05-11 08:40:02 +0200

    Implement CategoryQuesions

--- TriviaRefactoringKata/src/main/java/com/workshop/refactoring/CategoryQuestions.java
+++ TriviaRefactoringKata/src/main/java/com/workshop/refactoring/CategoryQuestions.java
@@ -5,22 +5,29 @@ import java.util.List;
 
 public class CategoryQuestions {
 
-    public CategoryQuestions(String name, LinkedList questions, List<Integer> places) {
+    private final String name;
+    private final LinkedList questions;
+    private final List<Integer> places;
 
+    public CategoryQuestions(String name, LinkedList questions, List<Integer> places) {
+        this.name = name;
+        this.questions = questions;
+        this.places = places;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public void addQuestion(String question) {
+        questions.addLast(question);
     }
 
     public boolean contains(int place) {
-        return false;
+        return places.contains(place);
     }
 
     public Object nextQuestion() {
-        return null;
+        return questions.removeFirst();
     }
 }
