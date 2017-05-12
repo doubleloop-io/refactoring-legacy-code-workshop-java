6e9a6e0a4c02a48d64a172fcadea4df83e8df023
Matteo Baglini <matteo.baglini@gmail.com>
2017-05-12 02:07:09 +0200

    Mark as package-private CategoryQuestions's public methods

--- TriviaRefactoringKata/src/main/java/com/workshop/refactoring/CategoryQuestions.java
+++ TriviaRefactoringKata/src/main/java/com/workshop/refactoring/CategoryQuestions.java
@@ -4,36 +4,36 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class CategoryQuestions {
+class CategoryQuestions {
 
     private final String name;
     private final LinkedList questions;
     private final List<Integer> places;
 
-    public CategoryQuestions(String name) {
+    CategoryQuestions(String name) {
         this.name = name;
         this.questions = new LinkedList();
         this.places = new ArrayList<>();
     }
 
-    public String getName() {
+    String getName() {
         return name;
     }
 
-    public void addQuestion(String question) {
+    void addQuestion(String question) {
         questions.addLast(question);
     }
 
-    public boolean isPlacedOn(int place) {
+    boolean isPlacedOn(int place) {
         return places.contains(place);
     }
 
-    public Object nextQuestion() {
+    Object nextQuestion() {
         if (questions.size() == 0) throw new EndOfQuestionsException(name);
         return questions.removeFirst();
     }
 
-    public void placeOn(List<Integer> places) {
+    void placeOn(List<Integer> places) {
         this.places.addAll(places);
     }
 }
